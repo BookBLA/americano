2dadd6a079693fb87f884b55c4b1f34f75fe4027
banggeunho <panggeunho@gmail.com>
2024-01-07 22:48:43 +0900

    feat: testcontroller, dto 수정

--- src/main/java/com/bookbla/americano/domain/test/controller/TestController.java
+++ src/main/java/com/bookbla/americano/domain/test/controller/TestController.java
@@ -5,10 +5,7 @@ import com.bookbla.americano.domain.test.controller.dto.request.TestRequestDTO;
 import com.bookbla.americano.domain.test.controller.dto.response.TestResponseDTO;
 import com.bookbla.americano.domain.test.service.TestService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/test")
@@ -23,7 +20,7 @@ public class TestController {
     }
 
     @PostMapping("")
-    public TestResponseDTO testSave(TestRequestDTO requestDTO) {
+    public TestResponseDTO testSave(@RequestBody TestRequestDTO requestDTO) {
         return testService.test(requestDTO);
     }
 
--- src/main/java/com/bookbla/americano/domain/test/controller/dto/request/TestRequestDTO.java
+++ src/main/java/com/bookbla/americano/domain/test/controller/dto/request/TestRequestDTO.java
@@ -1,18 +1,16 @@
 package com.bookbla.americano.domain.test.controller.dto.request;
 
 import com.bookbla.americano.domain.test.repository.entity.TestEntity;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Builder
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class TestRequestDTO {
 
-    private final String contents;
+    private String contents;
 
     public static TestEntity toEntity(TestRequestDTO requestDTO) {
         return TestEntity.builder()
