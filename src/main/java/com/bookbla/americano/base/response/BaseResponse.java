e52fa556e0775ea026e72d706ba7d6630825194a
iamjooon2 <iamjooon2@gmail.com>
2024-01-10 16:28:50 +0900

    refactor: 기본 응답 내 message 필드 제거

--- src/main/java/com/bookbla/americano/base/response/BaseResponse.java
+++ src/main/java/com/bookbla/americano/base/response/BaseResponse.java
@@ -12,14 +12,12 @@ public class BaseResponse<T> {
     @JsonProperty("isSuccess")
     private final boolean isSuccess;
     private final int code;
-    private final String message;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private final T result;
 
     public BaseResponse(T result) {
         this.isSuccess = true;
-        this.message = "응답 성공!";
         this.code = 200;
         this.result = result;
     }
