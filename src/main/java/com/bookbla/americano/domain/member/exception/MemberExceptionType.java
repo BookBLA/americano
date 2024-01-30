536a6050b17ad1dd81b85e269ef5620355cf009b
iamjooon2 <iamjooon2@gmail.com>
2024-01-30 19:17:24 +0900

    fix: MemberExceptionType HttpStatus코드 추가

--- src/main/java/com/bookbla/americano/domain/member/exception/MemberExceptionType.java
+++ src/main/java/com/bookbla/americano/domain/member/exception/MemberExceptionType.java
@@ -3,17 +3,18 @@ package com.bookbla.americano.domain.member.exception;
 import com.bookbla.americano.base.exception.ExceptionType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum MemberExceptionType implements ExceptionType {
 
-    EMAIL_NOT_VALID("1111", "형식에 맞지 않는 이메일입니다", "asd".repeat(3)),
-    MEMBER_TYPE_NOT_VALID("4003", "존재하지 않는 유저 타입입니다~~", "asd".repeat(3)),
+    EMAIL_NOT_VALID(HttpStatus.BAD_REQUEST, "4001", "형식에 맞지 않는 이메일입니다"),
+    MEMBER_TYPE_NOT_VALID(HttpStatus.NOT_FOUND, "4003", "존재하지 않는 유저 타입입니다~~"),
     ;
 
+    private final HttpStatus httpStatus;
     private final String errorCode;
     private final String message;
-    private final String stackTrace;
 
 }
