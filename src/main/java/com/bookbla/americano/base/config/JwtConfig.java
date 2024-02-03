786a9a47373707368476532b23c260f1a1cf5d4b
banggeunho <panggeunho@gmail.com>
2024-02-04 01:16:55 +0900

    fix: github action os 설정 변경

--- src/main/java/com/bookbla/americano/base/config/JwtConfig.java
+++ src/main/java/com/bookbla/americano/base/config/JwtConfig.java
@@ -4,11 +4,11 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
-@Component
+@Configuration
 @RequiredArgsConstructor
 @ConfigurationProperties(prefix = "jwt")
 public class JwtConfig {
