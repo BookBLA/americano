d96165d1ff6eb4144834ff51577810dc929d1b59
iamjooon2 <iamjooon2@gmail.com>
2024-02-08 01:15:50 +0900

    feat: 회원의 스타일을 업데이트 하는 API 추가

--- src/main/java/com/bookbla/americano/domain/member/controller/MemberController.java
+++ src/main/java/com/bookbla/americano/domain/member/controller/MemberController.java
@@ -3,9 +3,11 @@ package com.bookbla.americano.domain.member.controller;
 import com.bookbla.americano.domain.member.controller.dto.StylesResponse;
 import com.bookbla.americano.domain.member.service.MemberStyleService;
 import com.bookbla.americano.domain.member.service.dto.MemberStyleResponse;
+import com.bookbla.americano.domain.member.service.dto.MemberStyleUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +28,13 @@ public class MemberController {
     public ResponseEntity<MemberStyleResponse> readMemberStyle(Long memberId) {
         MemberStyleResponse memberStyleResponse = memberStyleService.readMemberStyle(memberId);
         return ResponseEntity.ok(memberStyleResponse);
+    }
+
+    // member 식별자를 어디서 받아올까...
+    @PutMapping("/styles")
+    public ResponseEntity<Void> updateMemberStyle(Long memberId, MemberStyleUpdateRequest memberStyleUpdateRequest) {
+        memberStyleService.updateMemberStyle(memberId, memberStyleUpdateRequest);
+        return ResponseEntity.noContent().build();
     }
 
 }
