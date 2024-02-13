47edde125596114f900bf94ca8f35c7e113fbc59
GinToFim <shlf6673@gmail.com>
2024-02-14 02:24:32 +0900

    feat: Member 리포지토리 추가

---
+++ src/main/java/com/bookbla/americano/domain/member/repository/MemberAuthRepository.java
@@ -0,0 +1,8 @@
+package com.bookbla.americano.domain.member.repository;
+
+import com.bookbla.americano.domain.member.repository.entity.MemberAuth;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberAuthRepository extends JpaRepository<MemberAuth, Long> {
+
+}
