8b099b825af5f8d7c94693a1fc1a31aae9c07eed
JOOHYUN_KIM <kjh99723@gmail.com>
2024-02-16 18:14:23 +0900

    refactor: Cognitive Complexity 감소를 위해 코드 수정

--- src/main/java/com/bookbla/americano/domain/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/bookbla/americano/domain/member/service/impl/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package com.bookbla.americano.domain.member.service.impl;
 import com.bookbla.americano.base.exception.BaseException;
 import com.bookbla.americano.domain.member.controller.dto.request.MemberBookProfileRequestDto;
 import com.bookbla.americano.domain.member.controller.dto.response.MemberBookProfileResponseDto;
+import com.bookbla.americano.domain.member.enums.Gender;
 import com.bookbla.americano.domain.member.exception.MemberExceptionType;
 import com.bookbla.americano.domain.member.repository.MemberProfileRepository;
 import com.bookbla.americano.domain.member.repository.MemberRepository;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -41,54 +43,42 @@ public class MemberServiceImpl implements MemberService {
             .updateMemberType(request.getMemberType());
     }
 
+	@Override
+    public Member getMemberById(Long memberId) {
+        return memberRepository.getByIdOrThrow(memberId);
+    }
+    
     @Override
     @Transactional(readOnly = true)
     public List<MemberBookProfileResponseDto> findSameBookMembers(MemberBookProfileRequestDto requestDto) {
         List<MemberBookProfileResponseDto> allResult = repository.searchSameBookMember(requestDto);
-
+        // 탐색 결과가 없을 때, EMPTY_MEMBER_BOOK Exception(해당 사용자의 선호 책도 없음)
         if (allResult.isEmpty()) {
             throw new BaseException(MemberExceptionType.EMPTY_MEMBER_BOOK);
         }
+        // 해당 사용자의 선호 책
         List<MemberBookProfileResponseDto> userBookProfile = new ArrayList<>();
+        List<MemberBookProfileResponseDto> othersBookProfile = new ArrayList<>(allResult);
         for (MemberBookProfileResponseDto i : allResult) {
             if (i.getMemberId() == requestDto.getMemberId()) {
-                if (i.isBookIsRepresentative())
-                    userBookProfile.add(0, i);
-                else
-                    userBookProfile.add(i);
+                addBookProfileToList(userBookProfile, i);
+                othersBookProfile.remove(i);
             }
         }
-
+        // 해당 사용자의 선호책이 없을 때, EMPTY_MEMBER_BOOK Exception
         if (userBookProfile.isEmpty()) {
             throw new BaseException(MemberExceptionType.EMPTY_MEMBER_BOOK);
         }
 
-        Map<Long, MemberBookProfileResponseDto> selected = new HashMap<>();
         List<MemberBookProfileResponseDto> result = new ArrayList<>();
+        Map<Long, MemberBookProfileResponseDto> selected = new HashMap<>();
         int afterRepresentativeIdx = -1;
         for (MemberBookProfileResponseDto i : userBookProfile) {
-            for (MemberBookProfileResponseDto j : allResult) {
-                if (i.getMemberId() != j.getMemberId() && i.getBookId() == j.getBookId()) {
-                    if (selected.containsKey(j.getMemberId()) && result.indexOf(selected.get(j.getMemberId())) >= afterRepresentativeIdx && j.isBookIsRepresentative()) {
-                        result.add(afterRepresentativeIdx, j);
-                        result.remove(selected.get(j.getMemberId()));
-                        selected.remove(j.getMemberId());
-                        selected.put(j.getMemberId(), j);
-                    } else if (!selected.containsKey(j.getMemberId())) {
-                        if (requestDto.getGender() != null) {
-                            if (requestDto.getGender() == j.getMemberGender()) {
-                                if (j.isBookIsRepresentative() && i.isBookIsRepresentative())
-                                    result.add(0, j);
-                                else
-                                    result.add(j);
-                                selected.put(j.getMemberId(), j);
-                            }
-                        } else {
-                            result.add(j);
-                            selected.put(j.getMemberId(), j);
-                        }
-                    }
-                }
+            List<MemberBookProfileResponseDto> bookProfiles = othersBookProfile.stream()
+                    .filter(bp -> i.getBookId() == bp.getBookId())
+                    .collect(Collectors.toList());
+            for (MemberBookProfileResponseDto j : bookProfiles) {
+                addBookProfileToList(result, i, j, selected, afterRepresentativeIdx, requestDto.getGender());
             }
             if (afterRepresentativeIdx == -1)
                 afterRepresentativeIdx = result.size();
@@ -99,5 +89,36 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public Member getMemberById(Long memberId) {
         return memberRepository.getByIdOrThrow(memberId);
+    }
+
+    private void addBookProfileToList(List<MemberBookProfileResponseDto> list, MemberBookProfileResponseDto i) {
+        if (i.isBookIsRepresentative())
+            list.add(0, i);
+        else
+            list.add(i);
+    }
+
+    private void addBookProfileToList(List<MemberBookProfileResponseDto> result, MemberBookProfileResponseDto i, MemberBookProfileResponseDto j, Gender gender) {
+        if (gender == null) {
+            result.add(j);
+        } else if (gender == j.getMemberGender()) {
+            if (j.isBookIsRepresentative() && i.isBookIsRepresentative())
+                result.add(0, j);
+            else
+                result.add(j);
+        }
+    }
+
+    private void addBookProfileToList(List<MemberBookProfileResponseDto> result, MemberBookProfileResponseDto i, MemberBookProfileResponseDto j,
+                                      Map<Long, MemberBookProfileResponseDto> selected, int afterRepresentativeIdx, Gender gender) {
+        if (selected.containsKey(j.getMemberId()) && j.isBookIsRepresentative() && result.indexOf(selected.get(j.getMemberId())) >= afterRepresentativeIdx) {
+            result.add(afterRepresentativeIdx, j);
+            result.remove(selected.get(j.getMemberId()));
+            selected.remove(j.getMemberId());
+            selected.put(j.getMemberId(), j);
+        } else if (!selected.containsKey(j.getMemberId())) {
+            addBookProfileToList(result, i, j, gender);
+            selected.put(j.getMemberId(), j);
+        }
     }
 }
