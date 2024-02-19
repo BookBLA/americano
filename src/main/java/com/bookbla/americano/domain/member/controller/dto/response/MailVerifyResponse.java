f7db0c08d7beff54f34bc702c737dc46b552ffdf
GinToFim <shlf6673@gmail.com>
2024-02-19 19:34:45 +0900

    feat: 이메일 인증 API 추가

---
+++ src/main/java/com/bookbla/americano/domain/member/controller/dto/response/MailVerifyResponse.java
@@ -0,0 +1,13 @@
+package com.bookbla.americano.domain.member.controller.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class MailVerifyResponse {
+
+    private String message;
+}
