1523aa9c5a8051e54656090d75ef276cdcb5b77a
iamjooon2 <iamjooon2@gmail.com>
2024-02-19 23:20:21 +0900

    refactor: 협의한 요청/응답 형식에 맞추어 API 스펙 변경

--- src/main/java/com/bookbla/americano/domain/auth/controller/dto/request/LoginRequestDto.java
+++ src/main/java/com/bookbla/americano/domain/auth/controller/dto/request/LoginRequestDto.java
@@ -1,18 +1,17 @@
 package com.bookbla.americano.domain.auth.controller.dto.request;
 
+import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class LoginRequestDto {
 
-    @NotNull(message = "인가코드가 입력되지 않았습니다.")
+    @NotNull(message = "인가코드가 입력되지 않았습니다")
     private String authCode;
 
 }
