403b026d9cbf8039fa1fd0ba73289ba25fb1b7f8
iamjooon2 <iamjooon2@gmail.com>
2024-02-20 00:04:20 +0900

    feat: 회원의 스타일을 생성 API 추가

--- src/main/java/com/bookbla/americano/domain/member/controller/MemberStyleController.java
+++ src/main/java/com/bookbla/americano/domain/member/controller/MemberStyleController.java
@@ -1,12 +1,15 @@
 package com.bookbla.americano.domain.member.controller;
 
+import com.bookbla.americano.domain.member.controller.dto.request.MemberStyleCreateRequest;
 import com.bookbla.americano.domain.member.controller.dto.response.StylesResponse;
 import com.bookbla.americano.domain.member.service.MemberStyleService;
 import com.bookbla.americano.domain.member.controller.dto.response.MemberStyleResponse;
 import com.bookbla.americano.domain.member.controller.dto.request.MemberStyleUpdateRequest;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +24,14 @@ public class MemberStyleController {
     @GetMapping
     public ResponseEntity<StylesResponse> readStyles() {
         return ResponseEntity.ok(memberStyleService.readStyles());
+    }
+
+    @PostMapping
+    public ResponseEntity<MemberStyleResponse> createMemberStyle(Long memberId, MemberStyleCreateRequest memberStyleCreateRequest) {
+        MemberStyleResponse memberStyleResponse = memberStyleService.createMemberStyle(memberId,
+                memberStyleCreateRequest);
+        return ResponseEntity.created(URI.create(memberStyleResponse.getMemberStyleId().toString()))
+                .body(memberStyleResponse);
     }
 
     // member 식별자를 어디서 받아올까...
