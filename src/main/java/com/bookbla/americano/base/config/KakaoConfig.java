f5e580effc6644139c89dc21234a318cb52b53ad
GinToFim <shlf6673@gmail.com>
2024-02-24 17:19:09 +0900

    feat: 카카오 로그인 설정

---
+++ src/main/java/com/bookbla/americano/base/config/KakaoConfig.java
@@ -0,0 +1,21 @@
+package com.bookbla.americano.base.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "oauth.kakao")
+public class KakaoConfig {
+
+    private String clientId;
+    private String clientSecret;
+    private String redirectUri;
+    private String tokenUri;
+    private String resourceUri;
+    private String grantType;
+
+}
