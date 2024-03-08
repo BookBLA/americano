d1192e3ed6c168ebb8c6fefbc9dc01503479e516
iamjooon2 <iamjooon2@gmail.com>
2024-03-08 22:27:42 +0900

    refactor: 기존 service 인터페이스와 구현체로 분리

--- src/main/java/com/bookbla/americano/domain/member/service/MemberStyleService.java
+++ src/main/java/com/bookbla/americano/domain/member/service/MemberStyleService.java
@@ -1,56 +1,6 @@
 package com.bookbla.americano.domain.member.service;
 
-import com.bookbla.americano.domain.member.repository.entity.Member;
-import com.bookbla.americano.domain.member.repository.entity.MemberStyle;
-import com.bookbla.americano.domain.member.controller.dto.request.MemberStyleCreateRequest;
-import com.bookbla.americano.domain.member.repository.MemberRepository;
-import com.bookbla.americano.domain.member.repository.MemberStyleRepository;
-import com.bookbla.americano.domain.member.controller.dto.response.MemberStyleResponse;
-import com.bookbla.americano.domain.member.controller.dto.request.MemberStyleUpdateRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class MemberStyleService {
-
-    private final MemberRepository memberRepository;
-    private final MemberStyleRepository memberStyleRepository;
-
-    @Transactional(readOnly = true)
-    public MemberStyleResponse readMemberStyle(Long memberId) {
-        Member member = memberRepository.getByIdOrThrow(memberId);
-        MemberStyle memberStyle = memberStyleRepository.getByMemberOrThrow(member);
-
-        return MemberStyleResponse.of(member, memberStyle);
-    }
-
-    public MemberStyleResponse createMemberStyle(Long memberId, MemberStyleCreateRequest memberStyleCreateRequest) {
-        Member member = memberRepository.getByIdOrThrow(memberId);
-        MemberStyle memberStyle = memberStyleCreateRequest.toMemberStyleWith(member);
-
-        MemberStyle savedMemberStyle = memberStyleRepository.save(memberStyle);
-
-        return MemberStyleResponse.of(member, savedMemberStyle);
-    }
-
-    public void updateMemberStyle(Long memberId, MemberStyleUpdateRequest memberStyleUpdateRequest) {
-        Member member = memberRepository.getByIdOrThrow(memberId);
-        MemberStyle memberStyle = memberStyleRepository.getByMemberOrThrow(member);
-
-        update(memberStyle, memberStyleUpdateRequest);
-    }
-
-    private void update(MemberStyle memberStyle, MemberStyleUpdateRequest memberStyleUpdateRequest) {
-        memberStyle.updateMbti(memberStyleUpdateRequest.getMbti());
-        memberStyle.updateDrinkType(memberStyleUpdateRequest.getDrinkType());
-        memberStyle.updateDateCostType(memberStyleUpdateRequest.getDateCostType());
-        memberStyle.updateSmokeType(memberStyleUpdateRequest.getSmokeType());
-        memberStyle.updateContactType(memberStyleUpdateRequest.getContactType());
-        memberStyle.updateDateStyleType(memberStyleUpdateRequest.getDateStyleType());
-        memberStyle.updateJustFriendType(memberStyleUpdateRequest.getJustFriendType());
-    }
+public interface MemberStyleService {
 
 }
--- src/main/java/com/bookbla/americano/domain/member/service/StyleService.java
+++ src/main/java/com/bookbla/americano/domain/member/service/StyleService.java
@@ -1,25 +1,8 @@
 package com.bookbla.americano.domain.member.service;
 
 import com.bookbla.americano.domain.member.controller.dto.response.StylesResponse;
-import com.bookbla.americano.domain.member.enums.ContactType;
-import com.bookbla.americano.domain.member.enums.DateCostType;
-import com.bookbla.americano.domain.member.enums.DateStyleType;
-import com.bookbla.americano.domain.member.enums.DrinkType;
-import com.bookbla.americano.domain.member.enums.JustFriendType;
-import com.bookbla.americano.domain.member.enums.SmokeType;
-import org.springframework.stereotype.Service;
 
-@Service
-public class StyleService {
+public interface StyleService {
 
-    public StylesResponse readStyles() {
-        return StylesResponse.of(
-                SmokeType.getValues(),
-                DrinkType.getValues(),
-                ContactType.getValues(),
-                DateStyleType.getValues(),
-                DateCostType.getValues(),
-                JustFriendType.getValues()
-        );
-    }
+    StylesResponse readStyles();
 }
