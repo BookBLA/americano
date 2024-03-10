b93349b7309a14f4f94395287de67dea19a866e4
iamjooon2 <iamjooon2@gmail.com>
2024-03-11 04:02:22 +0900

    refactor: BookProvider 추상회 제거

--- src/main/java/com/bookbla/americano/domain/book/service/BookProvider.java
+++
@@ -1,9 +0,0 @@
-package com.bookbla.americano.domain.book.service;
-
-import com.bookbla.americano.domain.book.service.dto.BookSearchResponses;
-
-public interface BookProvider {
-
-    BookSearchResponses searchBooks(String text, int size, int page);
-
-}
--- src/main/java/com/bookbla/americano/domain/book/service/impl/BookServiceImpl.java
+++ src/main/java/com/bookbla/americano/domain/book/service/impl/BookServiceImpl.java
@@ -1,20 +1,29 @@
 package com.bookbla.americano.domain.book.service.impl;
 
+import com.bookbla.americano.domain.book.infra.kakao.KakaoBookClient;
+import com.bookbla.americano.domain.book.infra.kakao.dto.KakaoBookResponse;
+import com.bookbla.americano.domain.book.infra.kakao.dto.KakaoTokenConfig;
 import com.bookbla.americano.domain.book.service.dto.BookSearchResponses;
-import com.bookbla.americano.domain.book.service.BookProvider;
 import com.bookbla.americano.domain.book.service.BookService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
 
-    private final BookProvider bookProvider;
+    private static final String AUTH_TYPE = "KakaoAK ";
+
+    private final KakaoBookClient kakaoBookClient;
+    private final String token;
+
+    public BookServiceImpl(KakaoBookClient kakaoBookClient, KakaoTokenConfig kakaoTokenConfig) {
+        this.kakaoBookClient = kakaoBookClient;
+        this.token = kakaoTokenConfig.getToken();
+    }
 
     @Override
     public BookSearchResponses searchBook(String text, int size, int page) {
-        return bookProvider.searchBooks(text, size, page);
+        String tokenValue = AUTH_TYPE + token;
+        KakaoBookResponse kakaoBookResponse = kakaoBookClient.getBookInformation(tokenValue, text, size, page);
+        return kakaoBookResponse.toBookSearchResponsesWith(page);
     }
-
 }
