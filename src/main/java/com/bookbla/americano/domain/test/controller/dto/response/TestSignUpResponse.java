dc78dc548a8c16bcfd562d9bd26e8275e2920ad5
iamjooon2 <iamjooon2@gmail.com>
2024-03-23 19:22:30 +0900

    fix: 서브모듈 업데이트

---
+++ src/main/java/com/bookbla/americano/domain/test/controller/dto/response/TestSignUpResponse.java
@@ -0,0 +1,13 @@
+package com.bookbla.americano.domain.test.controller.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class TestSignUpResponse {
+
+    private final String accessToken;
+
+}
