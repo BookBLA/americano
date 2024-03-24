3277699f1676d3ab37cb86d754a27b6e0b8e2c01
GinToFim <shlf6673@gmail.com>
2024-03-24 19:11:32 +0900

    feat: Swagger @parameter 기능 추가

--- src/main/java/com/bookbla/americano/domain/memberask/controller/MemberAskController.java
+++ src/main/java/com/bookbla/americano/domain/memberask/controller/MemberAskController.java
@@ -7,6 +7,7 @@ import com.bookbla.americano.domain.memberask.controller.dto.request.MemberAskCr
 import com.bookbla.americano.domain.memberask.controller.dto.request.MemberAskUpdateRequest;
 import com.bookbla.americano.domain.memberask.controller.dto.response.MemberAskResponse;
 import com.bookbla.americano.domain.memberask.service.MemberAskService;
+import io.swagger.v3.oas.annotations.Parameter;
 import java.net.URI;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,23 +28,28 @@ public class MemberAskController {
 
     @PostMapping
     public ResponseEntity<Void> createMemberAsk(
-            @User LoginUser loginUser, @RequestBody @Valid MemberAskCreateRequest memberAskCreateRequest
+        @Parameter(hidden = true) @User LoginUser loginUser,
+        @RequestBody @Valid MemberAskCreateRequest memberAskCreateRequest
     ) {
-        MemberAskResponse memberAskResponse = memberAskService.createMemberAsk(loginUser.getMemberId(), memberAskCreateRequest);
-        return ResponseEntity.created(URI.create(memberAskResponse.getMemberAskResponseId().toString()))
-                .build();
+        MemberAskResponse memberAskResponse = memberAskService.createMemberAsk(
+            loginUser.getMemberId(), memberAskCreateRequest);
+        return ResponseEntity.created(
+                URI.create(memberAskResponse.getMemberAskResponseId().toString()))
+            .build();
     }
 
     @GetMapping
-    public ResponseEntity<MemberAskResponse> readMemberAsk(@User LoginUser loginUser) {
-        MemberAskResponse memberAskResponse = memberAskService.readMemberAsk(loginUser.getMemberId());
+    public ResponseEntity<MemberAskResponse> readMemberAsk(
+        @Parameter(hidden = true) @User LoginUser loginUser) {
+        MemberAskResponse memberAskResponse = memberAskService.readMemberAsk(
+            loginUser.getMemberId());
         return ResponseEntity.ok(memberAskResponse);
     }
 
     @PutMapping
     public ResponseEntity<Void> updateMemberAsk(
-            @User LoginUser loginUser,
-            @RequestBody @Valid MemberAskUpdateRequest memberAskUpdateRequest) {
+        @Parameter(hidden = true) @User LoginUser loginUser,
+        @RequestBody @Valid MemberAskUpdateRequest memberAskUpdateRequest) {
         memberAskService.updateMemberAsk(loginUser.getMemberId(), memberAskUpdateRequest);
         return ResponseEntity.noContent().build();
     }
