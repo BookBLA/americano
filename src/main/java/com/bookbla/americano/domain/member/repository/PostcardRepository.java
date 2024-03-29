1716af1d492663869a69c23a87bf03f59e580b43
iamjooon2 <iamjooon2@gmail.com>
2024-03-29 19:58:48 +0900

    refactor: Repository 내 boolean 원시 타입으로 변경

--- src/main/java/com/bookbla/americano/domain/member/repository/PostcardRepository.java
+++ src/main/java/com/bookbla/americano/domain/member/repository/PostcardRepository.java
@@ -4,8 +4,6 @@ import com.bookbla.americano.domain.postcard.Postcard;
 import com.bookbla.americano.domain.postcard.PostcardStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface PostcardRepository extends JpaRepository<Postcard, Long> {
-    Boolean existsBySendMemberIdAndReciveMemberIdAndPostcardStatus(Long sendMemberId, Long reciveMemberId, PostcardStatus status);
+    boolean existsBySendMemberIdAndReciveMemberIdAndPostcardStatus(Long sendMemberId, Long reciveMemberId, PostcardStatus status);
 }
