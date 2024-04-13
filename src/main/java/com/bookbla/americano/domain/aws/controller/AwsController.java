4a032cae9568c342ae9b5afbf211a9cb3c47e74e
banggeunho <panggeunho@gmail.com>
2024-04-14 00:26:28 +0900

    feat: s3 컨트롤러 구현

---
+++ src/main/java/com/bookbla/americano/domain/aws/controller/AwsController.java
@@ -0,0 +1,33 @@
+package com.bookbla.americano.domain.aws.controller;
+
+import com.bookbla.americano.base.resolver.LoginUser;
+import com.bookbla.americano.base.resolver.User;
+import com.bookbla.americano.domain.aws.controller.dto.response.S3PresignedUrlResponse;
+import com.bookbla.americano.domain.aws.controller.enums.UploadType;
+import com.bookbla.americano.domain.aws.service.S3Service;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/aws")
+public class AwsController {
+
+    private final S3Service s3Service;
+
+    @GetMapping("/s3/presigned-url/{uploadType}")
+    public ResponseEntity<S3PresignedUrlResponse> getS3PresignedUrl(
+            @Parameter(hidden = true) @User LoginUser loginUser,
+            @PathVariable String uploadType,
+            @RequestParam String fileName
+    ) {
+        S3PresignedUrlResponse s3PresignedUrlResponse = S3PresignedUrlResponse.builder()
+                .presignedUrl(s3Service.getPreSignedUrl(UploadType.from(uploadType), fileName))
+                .build();
+
+        return ResponseEntity.ok(s3PresignedUrlResponse);
+    }
+}
+
