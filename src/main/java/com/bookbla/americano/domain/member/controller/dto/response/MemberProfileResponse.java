7f96d2f37e3713e9e4644e0d907670b5bed904a9
GinToFim <shlf6673@gmail.com>
2024-04-19 01:52:11 +0900

    fix: PhoneNumber 추가

--- src/main/java/com/bookbla/americano/domain/member/controller/dto/response/MemberProfileResponse.java
+++ src/main/java/com/bookbla/americano/domain/member/controller/dto/response/MemberProfileResponse.java
@@ -21,17 +21,20 @@ public class MemberProfileResponse {
     private String birthDate;
     private String schoolName;
     private String schoolEmail;
+    private String phoneNumber;
     private String gender;
     private String profileImageUrl;
     private String openKakaoRoomUrl;
     private String studentIdImageUrl;
 
-    public static MemberProfileResponse from(MemberProfile memberProfile) {
+    public static MemberProfileResponse from(Member member, MemberProfile memberProfile) {
         return MemberProfileResponse.builder()
+            .memberId(member.getId())
             .name(memberProfile.getName())
             .birthDate(memberProfile.getBirthDate().toString())
             .schoolName(memberProfile.getSchoolName())
             .schoolEmail(memberProfile.getSchoolEmail())
+            .phoneNumber(memberProfile.getPhoneNumber())
             .gender(memberProfile.getGender().name())
             .profileImageUrl(memberProfile.getProfileImageUrl())
             .openKakaoRoomUrl(memberProfile.getOpenKakaoRoomUrl())
