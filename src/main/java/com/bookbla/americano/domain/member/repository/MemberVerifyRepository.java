084c887e545c61795d921ee0f9dc5704d0f43839
iamjooon2 <iamjooon2@gmail.com>
2024-04-21 01:45:18 +0900

    feat: MemberVerify id로 못찾아 올 때 예외 던지도록 구현

--- src/main/java/com/bookbla/americano/domain/member/repository/MemberVerifyRepository.java
+++ src/main/java/com/bookbla/americano/domain/member/repository/MemberVerifyRepository.java
@@ -1,7 +1,9 @@
 package com.bookbla.americano.domain.member.repository;
 
+import com.bookbla.americano.base.exception.BaseException;
 import com.bookbla.americano.domain.member.enums.MemberVerifyStatus;
 import com.bookbla.americano.domain.member.enums.MemberVerifyType;
+import com.bookbla.americano.domain.member.exception.MemberVerifyExceptionType;
 import com.bookbla.americano.domain.member.repository.entity.MemberVerify;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,4 +12,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberVerifyRepository extends JpaRepository<MemberVerify, Long> {
 
     Page<MemberVerify> findByTypeAndStatus(MemberVerifyType type, MemberVerifyStatus status, Pageable pageable);
+
+    default MemberVerify getByIdOrThrow(Long memberVerifyId) {
+        return findById(memberVerifyId)
+                .orElseThrow(() -> new BaseException(MemberVerifyExceptionType.ID_NOT_FOUND));
+    }
 }
