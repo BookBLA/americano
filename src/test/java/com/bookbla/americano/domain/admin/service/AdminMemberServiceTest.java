b7c13bbb205c4de48806d83a7179e008e4dfab72
iamjooon2 <iamjooon2@gmail.com>
2024-04-20 22:13:53 +0900

    feat: 전체 회원 정보 조회 기능 구현

---
+++ src/test/java/com/bookbla/americano/domain/admin/service/AdminMemberServiceTest.java
@@ -0,0 +1,65 @@
+package com.bookbla.americano.domain.admin.service;
+
+import com.bookbla.americano.domain.admin.controller.dto.response.AdminMemberReadResponses;
+import com.bookbla.americano.domain.member.repository.MemberRepository;
+import com.bookbla.americano.domain.member.repository.entity.Member;
+import com.bookbla.americano.domain.member.repository.entity.MemberProfile;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+
+import static com.bookbla.americano.domain.member.enums.Gender.MALE;
+import static com.bookbla.americano.domain.member.enums.MemberStatus.APPROVAL;
+import static com.bookbla.americano.domain.member.enums.MemberStatus.COMPLETED;
+import static com.bookbla.americano.domain.member.enums.MemberStatus.STYLE_BOOK;
+import static com.bookbla.americano.domain.member.enums.MemberType.KAKAO;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+@SpringBootTest
+class AdminMemberServiceTest {
+
+    @Autowired
+    private AdminMemberService adminMemberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    void 회원들의_정보를_확인할_수_있다() {
+        // given
+        Member member1 = Member.builder()
+                .memberType(KAKAO)
+                .memberStatus(COMPLETED)
+                .oauthEmail("bookbla@bookbla.com")
+                .memberProfile(MemberProfile.builder().phoneNumber("01012345678").gender(MALE).schoolName("가천대").name("이준희").build())
+                .build();
+        memberRepository.save(member1);
+
+        Member member2 = Member.builder()
+                .memberType(KAKAO)
+                .memberStatus(STYLE_BOOK)
+                .oauthEmail("bookbla@bookbla.com")
+                .memberProfile(MemberProfile.builder().phoneNumber("01012345678").gender(MALE).schoolName("가천대").name("이준희").build())
+                .build();
+        memberRepository.save(member2);
+
+        Member member3 = Member.builder()
+                .memberType(KAKAO)
+                .memberStatus(APPROVAL)
+                .oauthEmail("bookbla@bookbla.com")
+                .memberProfile(MemberProfile.builder().phoneNumber("01012345678").gender(MALE).schoolName("가천대").name("이준희").build())
+                .build();
+        memberRepository.save(member3);
+
+        // when
+        AdminMemberReadResponses adminMemberReadResponses = adminMemberService.readMembers(PageRequest.of(0, 10));
+
+        // then
+        assertThat(adminMemberReadResponses.getData()).hasSize(3);
+    }
+}
