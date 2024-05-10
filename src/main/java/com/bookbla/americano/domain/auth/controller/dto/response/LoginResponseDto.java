c12a98c407e9afc8aa2d0327d33b6b492ccb0531
GinToFim <shlf6673@gmail.com>
2024-05-10 17:38:22 +0900

    fix: 멤버 상태 추가

--- src/main/java/com/bookbla/americano/domain/auth/controller/dto/response/LoginResponseDto.java
+++ src/main/java/com/bookbla/americano/domain/auth/controller/dto/response/LoginResponseDto.java
@@ -1,5 +1,6 @@
 package com.bookbla.americano.domain.auth.controller.dto.response;
 
+import com.bookbla.americano.domain.member.enums.MemberStatus;
 import com.bookbla.americano.domain.member.repository.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,11 +13,13 @@ public class LoginResponseDto {
     
     private final String accessToken;
     private final Long memberId;
+    private final String memberStatus;
 
     public static LoginResponseDto of(String accessToken, Member member) {
         return LoginResponseDto.builder()
                 .accessToken(accessToken)
                 .memberId(member.getId())
+                .memberStatus(member.getMemberStatus().name())
                 .build();
     }
 }
--- src/main/java/com/bookbla/americano/domain/auth/controller/dto/response/RejoinResponse.java
+++ src/main/java/com/bookbla/americano/domain/auth/controller/dto/response/RejoinResponse.java
@@ -1,6 +1,7 @@
 package com.bookbla.americano.domain.auth.controller.dto.response;
 
 import com.bookbla.americano.domain.auth.controller.dto.request.RejoinRequest;
+import com.bookbla.americano.domain.member.enums.MemberStatus;
 import com.bookbla.americano.domain.member.repository.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,11 +14,13 @@ public class RejoinResponse {
 
     private final String accessToken;
     private final Long memberId;
+    private final String memberStatus;
 
     public static RejoinResponse from(String accessToken, Member member) {
         return RejoinResponse.builder()
             .accessToken(accessToken)
             .memberId(member.getId())
+            .memberStatus(member.getMemberStatus().name())
             .build();
     }
 
