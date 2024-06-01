06248756e02517d49a0048b252a46c58e27b1a2c
banggeunho <panggeunho@gmail.com>
2024-06-02 00:39:43 +0900

    chore: 멤버 상태 수정 DTO NotBlank -> NotNULL

--- src/main/java/com/bookbla/americano/domain/member/controller/dto/request/MemberStatusUpdateRequest.java
+++ src/main/java/com/bookbla/americano/domain/member/controller/dto/request/MemberStatusUpdateRequest.java
@@ -1,7 +1,7 @@
 package com.bookbla.americano.domain.member.controller.dto.request;
 
 import com.bookbla.americano.domain.member.enums.MemberStatus;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,8 +12,8 @@ import lombok.NoArgsConstructor;
 @Getter
 public class MemberStatusUpdateRequest {
 
-    @NotBlank(message = "변경할 상태를 입력하지 않았습니다.")
+    @NotNull(message = "변경할 상태를 입력하지 않았습니다.")
     private MemberStatus memberStatus;
-    
+
     private String reason;
 }
