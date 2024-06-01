ba049f34ade7f7fb22cd261863c2642d8482a274
banggeunho <panggeunho@gmail.com>
2024-06-01 21:29:24 +0900

    feat: 멤버 상태 - 매칭 비활성화 추가

--- src/main/java/com/bookbla/americano/domain/member/enums/MemberStatus.java
+++ src/main/java/com/bookbla/americano/domain/member/enums/MemberStatus.java
@@ -11,7 +11,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum MemberStatus {
-    
+
     PROFILE("p", "회원 정보 입력이 필요"),
     APPROVAL("a", "회원 승인이 필요"),
     STYLE("s", "스타일 입력이 필요"),
@@ -19,22 +19,22 @@ public enum MemberStatus {
     COMPLETED("c", "회원 가입 모두 완료"),
     DELETED("d", "회원 탈퇴 상태"),
     BLOCKED("bl", "차단된 회원"),
-    ;
+    MATCHING_DISABLED("mb", "매칭 비활성화 회원");
 
     private final String value;
     private final String description;
 
     public static List<String> getValues() {
         return Arrays.stream(values())
-            .map(it -> it.value)
-            .collect(Collectors.toList());
+                .map(it -> it.value)
+                .collect(Collectors.toList());
     }
 
     public static MemberStatus from(String name) {
         return Arrays.stream(values())
-            .filter(it -> it.value.equalsIgnoreCase(name))
-            .findFirst()
-            .orElseThrow(() -> new BaseException(MemberExceptionType.MEMBER_STATUS_NOT_VALID));
+                .filter(it -> it.value.equalsIgnoreCase(name))
+                .findFirst()
+                .orElseThrow(() -> new BaseException(MemberExceptionType.MEMBER_STATUS_NOT_VALID));
     }
 
 }
