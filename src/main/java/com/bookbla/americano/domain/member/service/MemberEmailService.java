c0fccb7707ee4fd33342717dae3dd05018f37523
iamjooon2 <iamjooon2@gmail.com>
2024-06-04 17:36:59 +0900

    fix: 사용하지 않는 메서드 제거

--- src/main/java/com/bookbla/americano/domain/member/service/MemberEmailService.java
+++ src/main/java/com/bookbla/americano/domain/member/service/MemberEmailService.java
@@ -1,6 +1,5 @@
 package com.bookbla.americano.domain.member.service;
 
-import com.bookbla.americano.domain.member.controller.dto.request.EmailResendRequest;
 import com.bookbla.americano.domain.member.controller.dto.request.EmailSendRequest;
 import com.bookbla.americano.domain.member.controller.dto.request.EmailVerifyRequest;
 import com.bookbla.americano.domain.member.controller.dto.response.EmailResponse;
@@ -10,6 +9,4 @@ public interface MemberEmailService {
     EmailResponse sendEmail(Long memberId, EmailSendRequest emailSendRequest);
 
     EmailResponse verifyEmail(Long memberId, EmailVerifyRequest emailVerifyRequest);
-
-    EmailResponse resendEmail(Long memberId, EmailResendRequest emailResendRequest);
 }
