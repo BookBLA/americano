5466b30605482249de8fe27a02260614979ac06a
GinToFim <shlf6673@gmail.com>
2024-06-08 15:28:32 +0900

    fix: 탈퇴한 회원 탈퇴 시간 표시

--- src/main/java/com/bookbla/americano/domain/auth/service/AuthService.java
+++ src/main/java/com/bookbla/americano/domain/auth/service/AuthService.java
@@ -1,13 +1,9 @@
 package com.bookbla.americano.domain.auth.service;
 
 import com.bookbla.americano.domain.auth.controller.dto.request.LoginRequest;
-import com.bookbla.americano.domain.auth.controller.dto.request.RejoinRequest;
 import com.bookbla.americano.domain.auth.controller.dto.response.LoginResponse;
-import com.bookbla.americano.domain.auth.controller.dto.response.RejoinResponse;
 
 public interface AuthService {
 
     LoginResponse login(LoginRequest loginRequest, String oAuthType);
-
-    RejoinResponse rejoin(RejoinRequest rejoinRequest, String oAuthType);
 }
