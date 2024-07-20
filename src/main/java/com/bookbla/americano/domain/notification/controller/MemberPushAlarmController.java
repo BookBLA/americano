3df44fa40c3d6e67bfdfff9dc2557c98ca3cc751
GinToFim <shlf6673@gmail.com>
2024-07-20 15:42:17 +0900

    fix: request 어노테이션 추가

--- src/main/java/com/bookbla/americano/domain/notification/controller/MemberPushAlarmController.java
+++ src/main/java/com/bookbla/americano/domain/notification/controller/MemberPushAlarmController.java
@@ -8,12 +8,14 @@ import com.bookbla.americano.domain.notification.controller.dto.response.MemberP
 import com.bookbla.americano.domain.notification.service.MemberPushAlarmService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -55,7 +57,8 @@ public class MemberPushAlarmController {
     @Operation(summary = "해당 회원의 푸시 알림 설정 수정 API")
     @PostMapping("/settings")
     public ResponseEntity<PushAlarmSettingResponse> updatePushAlarmSetting(
-        @Parameter(hidden = true) @User LoginUser loginUser, PushAlarmSettingCreateRequest request
+        @Parameter(hidden = true) @User LoginUser loginUser,
+        @RequestBody @Valid PushAlarmSettingCreateRequest request
     ) {
         PushAlarmSettingResponse response = memberPushAlarmService.updatePushAlarmSetting(
             loginUser.getMemberId(), request);
