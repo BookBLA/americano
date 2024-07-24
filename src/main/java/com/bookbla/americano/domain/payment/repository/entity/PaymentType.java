74aa524a41a8249f483c06760aba263b6c847cbb
iamjooon2 <iamjooon2@gmail.com>
2024-07-24 17:32:42 +0900

    feat: PaymentExceptionType 정의

--- src/main/java/com/bookbla/americano/domain/payment/repository/entity/PaymentType.java
+++ src/main/java/com/bookbla/americano/domain/payment/repository/entity/PaymentType.java
@@ -2,6 +2,8 @@ package com.bookbla.americano.domain.payment.repository.entity;
 
 import java.util.Arrays;
 
+import com.bookbla.americano.base.exception.BaseException;
+import com.bookbla.americano.domain.payment.excpetion.PaymentExceptionType;
 import lombok.Getter;
 
 @Getter
@@ -15,6 +17,6 @@ public enum PaymentType {
         return Arrays.stream(values())
                 .filter(it -> it.name().equalsIgnoreCase(name))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("Invalid payment type: " + name));
+                .orElseThrow(() -> new BaseException(PaymentExceptionType.INVALID_PAY_TYPE));
     }
 }
