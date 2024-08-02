743de3255fb488bc68a1f7eb794bd78663750e33
iamjooon2 <iamjooon2@gmail.com>
2024-08-03 04:51:33 +0900

    feat: PaymentController 정의

---
+++ src/main/java/com/bookbla/americano/domain/payment/controller/dto/PaymentController.java
@@ -0,0 +1,35 @@
+package com.bookbla.americano.domain.payment.controller.dto;
+
+import com.bookbla.americano.base.resolver.LoginUser;
+import com.bookbla.americano.base.resolver.User;
+import com.bookbla.americano.domain.payment.controller.dto.request.PaymentBookmarkRequest;
+import com.bookbla.americano.domain.payment.controller.dto.response.PaymentPurchaseResponse;
+import com.bookbla.americano.domain.payment.service.PaymentService;
+import io.swagger.v3.oas.annotations.Parameter;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/{payType}")
+    public ResponseEntity<PaymentPurchaseResponse> orderBookmark(
+            @Parameter(hidden = true) @User LoginUser loginUser,
+            @Valid @RequestBody PaymentBookmarkRequest request,
+            @PathVariable String payType
+    ) {
+        PaymentPurchaseResponse paymentPurchaseResponse = paymentService.orderBookmark(payType, request, loginUser.getMemberId());
+        return ResponseEntity.ok(paymentPurchaseResponse);
+    }
+
+}
