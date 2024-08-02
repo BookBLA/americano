95585af282cb87b5219650bc9d3baee408546dc0
iamjooon2 <iamjooon2@gmail.com>
2024-08-03 03:44:34 +0900

    fix: 인앱결제 삭제

--- src/main/java/com/bookbla/americano/domain/payment/repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bookbla.americano.domain.payment.repository;
-
-import com.bookbla.americano.domain.payment.repository.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
-}
