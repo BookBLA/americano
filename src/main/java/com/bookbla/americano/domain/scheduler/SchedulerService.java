37ae6a8d6683b8339bcdeade436dbd54c961cf04
iamjooon2 <iamjooon2@gmail.com>
2024-08-09 18:21:58 +0900

    feat: 에러 메시지와 스택트레이스를 한번에 전달하도록 변경

--- src/main/java/com/bookbla/americano/domain/scheduler/SchedulerService.java
+++ src/main/java/com/bookbla/americano/domain/scheduler/SchedulerService.java
@@ -1,5 +1,9 @@
 package com.bookbla.americano.domain.scheduler;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.LocalDateTime;
+
 import com.bookbla.americano.base.log.discord.BookblaLogDiscord;
 import com.bookbla.americano.domain.member.repository.MemberEmailRepository;
 import com.bookbla.americano.domain.member.repository.MemberRepository;
@@ -10,14 +14,14 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-
 import static com.bookbla.americano.domain.scheduler.Crons.EVERY_4_AM;
 
 @RequiredArgsConstructor
 @Service
 @Slf4j
 public class SchedulerService {
+
+    private static final String CLRF = "\n\n";
 
     private final MemberRepository memberRepository;
     private final MemberEmailRepository memberEmailRepository;
@@ -34,12 +38,16 @@ public class SchedulerService {
 
         } catch (Exception e) {
             String txName = SchedulerService.class.getName() + "(deleteMemberEmailSchedule)";
-            String message = "임시 메일 테이블 초기화 작업이 실패하였습니다. 확인 부탁드립니다.";
+            String message = "임시 메일 테이블 초기화 작업이 실패하였습니다. 확인 부탁드립니다." + CLRF
+                    + e.getMessage() + CLRF
+                    + stackTraceToString(e);
 
             mailService.sendTransactionFailureEmail(txName, message);
             bookblaLogDiscord.sendMessage(message);
+
             log.debug("Exception in {}", SchedulerService.class.getName());
             log.error(e.toString());
+            log.error(stackTraceToString(e));
         }
     }
 
@@ -53,12 +61,23 @@ public class SchedulerService {
             memberRepository.deleteAllByDeletedAtBeforeAndMemberStatus(thirtyDaysAgo);
         } catch (Exception e) {
             String txName = SchedulerService.class.getName() + "(deleteMemberSchedule)";
-            String message = "멤버 테이블의 탈퇴한 멤버 삭제 작업이 실패하였습니다. 확인 부탁드립니다.";
+            String message = "멤버 테이블의 탈퇴한 멤버 삭제 작업이 실패하였습니다. 확인 부탁드립니다. " + CLRF
+                    + e.getMessage() + CLRF
+                    + stackTraceToString(e);
 
             mailService.sendTransactionFailureEmail(txName, message);
             bookblaLogDiscord.sendMessage(message);
+
             log.debug("Exception in {}", SchedulerService.class.getName());
             log.error(e.toString());
+            log.error(stackTraceToString(e));
         }
+    }
+
+    private String stackTraceToString(Exception e) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        return sw.toString();
     }
 }
