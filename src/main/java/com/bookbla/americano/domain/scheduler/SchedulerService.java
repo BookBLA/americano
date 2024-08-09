829cad3007c6e1c6231e7a600c7a98cdd8796db9
iamjooon2 <iamjooon2@gmail.com>
2024-08-09 18:20:28 +0900

    fix: 스케줄러 성공시 알림 발송 제거

--- src/main/java/com/bookbla/americano/domain/scheduler/SchedulerService.java
+++ src/main/java/com/bookbla/americano/domain/scheduler/SchedulerService.java
@@ -1,13 +1,11 @@
 package com.bookbla.americano.domain.scheduler;
 
 import com.bookbla.americano.base.log.discord.BookblaLogDiscord;
-import com.bookbla.americano.base.log.discord.dto.AdminLogEvent;
 import com.bookbla.americano.domain.member.repository.MemberEmailRepository;
 import com.bookbla.americano.domain.member.repository.MemberRepository;
 import com.bookbla.americano.domain.notification.service.MailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +23,6 @@ public class SchedulerService {
     private final MemberEmailRepository memberEmailRepository;
     private final MailService mailService;
     private final BookblaLogDiscord bookblaLogDiscord;
-    private final ApplicationEventPublisher applicationEventPublisher;
 
     @Transactional
     @Scheduled(cron = EVERY_4_AM, zone = "Asia/Seoul")
@@ -35,8 +32,6 @@ public class SchedulerService {
             LocalDateTime twoDaysAgo = currentDate.minusDays(2);
             memberEmailRepository.deleteMemberEmailSchedule(twoDaysAgo);
 
-            String message = LocalDateTime.now() + "부터 2일 사이의 임시 메일 테이블 삭제 작업이 성공하였습니다~.";
-            applicationEventPublisher.publishEvent(new AdminLogEvent(message));
         } catch (Exception e) {
             String txName = SchedulerService.class.getName() + "(deleteMemberEmailSchedule)";
             String message = "임시 메일 테이블 초기화 작업이 실패하였습니다. 확인 부탁드립니다.";
@@ -56,9 +51,6 @@ public class SchedulerService {
             LocalDateTime thirtyDaysAgo = currentDate.minusDays(30);
 
             memberRepository.deleteAllByDeletedAtBeforeAndMemberStatus(thirtyDaysAgo);
-
-            String message = LocalDateTime.now() + "부터 30일 사이의 탈퇴한 멤버 삭제 작업이 성공하였습니다";
-            applicationEventPublisher.publishEvent(new AdminLogEvent(message));
         } catch (Exception e) {
             String txName = SchedulerService.class.getName() + "(deleteMemberSchedule)";
             String message = "멤버 테이블의 탈퇴한 멤버 삭제 작업이 실패하였습니다. 확인 부탁드립니다.";
