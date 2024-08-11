fa60a41499e4aecb17f807bd56a35b28ceb18d10
iamjooon2 <iamjooon2@gmail.com>
2024-08-11 22:33:15 +0900

    refactor: BookService에서 KakaoBookClient를 사용하도록 변경

--- src/main/java/com/bookbla/americano/domain/book/service/BookService.java
+++ src/main/java/com/bookbla/americano/domain/book/service/BookService.java
@@ -1,28 +1,19 @@
 package com.bookbla.americano.domain.book.service;
 
-import com.bookbla.americano.domain.book.infra.kakao.KakaoBookApi;
+import com.bookbla.americano.domain.book.infra.kakao.KakaoBookClient;
 import com.bookbla.americano.domain.book.infra.kakao.dto.KakaoBookResponse;
 import com.bookbla.americano.domain.book.service.dto.BookSearchResponses;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookService {
 
-    private static final String AUTH_TYPE = "KakaoAK ";
-
-    private final KakaoBookApi kakaoBookApi;
-
-    @Value("${book.kakao.token}")
-    private String kakaoToken;
-
-    public BookService(KakaoBookApi kakaoBookApi) {
-        this.kakaoBookApi = kakaoBookApi;
-    }
+    private final KakaoBookClient kakaoBookClient;
 
     public BookSearchResponses searchBook(String text, int size, int page) {
-        String tokenValue = AUTH_TYPE + kakaoToken;
-        KakaoBookResponse kakaoBookResponse = kakaoBookApi.getBookInformation(tokenValue, text, size, page);
+        KakaoBookResponse kakaoBookResponse = kakaoBookClient.searchBook(text, size, page);
         return kakaoBookResponse.toBookSearchResponsesWith(page);
     }
 }
