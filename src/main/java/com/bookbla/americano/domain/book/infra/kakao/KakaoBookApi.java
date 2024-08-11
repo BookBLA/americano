519c1a29e7f699d28e278d796c91dba3ce831b34
iamjooon2 <iamjooon2@gmail.com>
2024-08-11 19:19:24 +0900

    refactor: 카카오 도서 API 관련 이름 변경

--- src/main/java/com/bookbla/americano/domain/book/infra/kakao/KakaoBookApi.java
+++ src/main/java/com/bookbla/americano/domain/book/infra/kakao/KakaoBookApi.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "kakao-book", url = "https://dapi.kakao.com/v3")
-public interface KakaoBookClient {
+public interface KakaoBookApi {
 
     @GetMapping("/search/book")
     KakaoBookResponse getBookInformation(
--- src/main/java/com/bookbla/americano/domain/book/service/BookService.java
+++ src/main/java/com/bookbla/americano/domain/book/service/BookService.java
@@ -1,9 +1,9 @@
 package com.bookbla.americano.domain.book.service;
 
-import com.bookbla.americano.domain.book.infra.kakao.KakaoBookClient;
+import com.bookbla.americano.domain.book.infra.kakao.KakaoBookApi;
 import com.bookbla.americano.domain.book.infra.kakao.dto.KakaoBookResponse;
-import com.bookbla.americano.domain.book.infra.kakao.dto.KakaoTokenConfig;
 import com.bookbla.americano.domain.book.service.dto.BookSearchResponses;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,17 +11,18 @@ public class BookService {
 
     private static final String AUTH_TYPE = "KakaoAK ";
 
-    private final KakaoBookClient kakaoBookClient;
-    private final String token;
+    private final KakaoBookApi kakaoBookApi;
 
-    public BookService(KakaoBookClient kakaoBookClient, KakaoTokenConfig kakaoTokenConfig) {
-        this.kakaoBookClient = kakaoBookClient;
-        this.token = kakaoTokenConfig.getToken();
+    @Value("${book.kakao.token}")
+    private String kakaoToken;
+
+    public BookService(KakaoBookApi kakaoBookApi) {
+        this.kakaoBookApi = kakaoBookApi;
     }
 
     public BookSearchResponses searchBook(String text, int size, int page) {
-        String tokenValue = AUTH_TYPE + token;
-        KakaoBookResponse kakaoBookResponse = kakaoBookClient.getBookInformation(tokenValue, text, size, page);
+        String tokenValue = AUTH_TYPE + kakaoToken;
+        KakaoBookResponse kakaoBookResponse = kakaoBookApi.getBookInformation(tokenValue, text, size, page);
         return kakaoBookResponse.toBookSearchResponsesWith(page);
     }
 }
