f66b4f763643576a17297961da2b391da96d6b83
iamjooon2 <iamjooon2@gmail.com>
2024-08-25 01:39:48 +0900

    feat: 사용자와 동일한 프로필을 조회하는 API 구현

--- src/main/java/com/bookbla/americano/domain/member/controller/docs/ProfileImageTypeControllerDocs.java
+++ src/main/java/com/bookbla/americano/domain/member/controller/docs/ProfileImageTypeControllerDocs.java
@@ -1,7 +1,10 @@
 package com.bookbla.americano.domain.member.controller.docs;
 
+import com.bookbla.americano.base.resolver.LoginUser;
+import com.bookbla.americano.base.resolver.User;
 import com.bookbla.americano.domain.member.controller.dto.response.ProfileImageTypeReadResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -15,4 +18,10 @@ public interface ProfileImageTypeControllerDocs {
     @GetMapping
     ResponseEntity<ProfileImageTypeReadResponse> readProfileImageTypes();
 
+    @Operation(summary = "사용자와 같은 성별의 기본 프로필 설정 값들을 읽어들입니다.")
+    @ApiResponse(responseCode = "200")
+    @GetMapping
+    ResponseEntity<ProfileImageTypeReadResponse> readMemberProfileGenderTypes(
+            @Parameter(hidden = true) @User LoginUser loginUser
+    );
 }
