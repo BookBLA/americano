778f3a77c7f9c21e2bfeb88601dbdc06ab993591
iamjooon2 <iamjooon2@gmail.com>
2024-08-25 02:04:05 +0900

    fix: API 명세 변경

--- src/main/java/com/bookbla/americano/domain/member/controller/ProfileImageTypeController.java
+++ src/main/java/com/bookbla/americano/domain/member/controller/ProfileImageTypeController.java
@@ -1,6 +1,5 @@
 package com.bookbla.americano.domain.member.controller;
 
-
 import com.bookbla.americano.base.resolver.LoginUser;
 import com.bookbla.americano.base.resolver.User;
 import com.bookbla.americano.domain.member.controller.docs.ProfileImageTypeControllerDocs;
@@ -9,22 +8,20 @@ import com.bookbla.americano.domain.member.service.ProfileImageTypeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/profile-image-types")
 public class ProfileImageTypeController implements ProfileImageTypeControllerDocs {
 
     private final ProfileImageTypeService profileImageTypeService;
 
-    @GetMapping
+    @GetMapping("/profile-image-types")
     public ResponseEntity<ProfileImageTypeReadResponse> readProfileImageTypes() {
         return ResponseEntity.ok(profileImageTypeService.readAll());
     }
 
-    @GetMapping("/me")
+    @GetMapping("/members/me/profile-image-types")
     public ResponseEntity<ProfileImageTypeReadResponse> readMemberProfileGenderTypes(
             @User LoginUser loginUser
     ) {
