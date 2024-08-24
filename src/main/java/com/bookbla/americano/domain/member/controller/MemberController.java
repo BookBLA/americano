199568e7c22dacdf7c748c7164ff4a32c59e299f
iamjooon2 <iamjooon2@gmail.com>
2024-08-25 03:33:13 +0900

    feat: 애드몹 시청 API 구현

--- src/main/java/com/bookbla/americano/domain/member/controller/MemberController.java
+++ src/main/java/com/bookbla/americano/domain/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.bookbla.americano.domain.member.controller;
 
+import java.util.List;
+
 import com.bookbla.americano.base.resolver.LoginUser;
 import com.bookbla.americano.base.resolver.User;
 import com.bookbla.americano.domain.member.controller.dto.request.MemberBookProfileRequestDto;
@@ -13,7 +15,6 @@ import com.bookbla.americano.domain.member.service.MemberProfileService;
 import com.bookbla.americano.domain.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -39,12 +40,12 @@ public class MemberController {
     private final MemberProfileService memberProfileService;
 
     @Operation(summary = "사용자 정보 조회 API",
-        description = "사용자의 oauth 정보, 타입, 상태 등을 조회<br>"
-            + "memberType : [APPLE, KAKAO, ADMIN]<br>"
-            + "memberStatus : [PROFILE, APPROVAL, STYLE, BOOK, COMPLETED, DELETED, BLOCKED, MATCHING_DISABLED, REPORTED]")
+            description = "사용자의 oauth 정보, 타입, 상태 등을 조회<br>"
+                    + "memberType : [APPLE, KAKAO, ADMIN]<br>"
+                    + "memberStatus : [PROFILE, APPROVAL, STYLE, BOOK, COMPLETED, DELETED, BLOCKED, MATCHING_DISABLED, REPORTED]")
     @GetMapping
     public ResponseEntity<MemberResponse> readMember(
-        @Parameter(hidden = true) @User LoginUser loginUser) {
+            @Parameter(hidden = true) @User LoginUser loginUser) {
         MemberResponse memberResponse = memberService.readMember(loginUser.getMemberId());
         return ResponseEntity.ok(memberResponse);
     }
@@ -52,49 +53,57 @@ public class MemberController {
     @Operation(summary = "멤버 상태 업데이트 API")
     @PostMapping("/status")
     public ResponseEntity<MemberStatusResponse> updateMemberStatus(
-        @Parameter(hidden = true) @User LoginUser loginUser,
-        @RequestBody @Valid MemberStatusUpdateRequest memberStatusUpdateRequest
+            @Parameter(hidden = true) @User LoginUser loginUser,
+            @RequestBody @Valid MemberStatusUpdateRequest memberStatusUpdateRequest
     ) {
         MemberStatusResponse memberStatusResponse = memberService.updateStatus(loginUser.getMemberId(), memberStatusUpdateRequest);
         return ResponseEntity.ok(memberStatusResponse);
     }
 
     @Operation(summary = "사용자 계정 탈퇴 API",
-        description = "탈퇴 시점(deletedAt)을 저장한 뒤에 30일 뒤에 회원 정보 삭제")
+            description = "탈퇴 시점(deletedAt)을 저장한 뒤에 30일 뒤에 회원 정보 삭제")
     @DeleteMapping
     public ResponseEntity<MemberDeleteResponse> deleteMember(
-        @Parameter(hidden = true) @User LoginUser loginUser) {
+            @Parameter(hidden = true) @User LoginUser loginUser) {
         MemberDeleteResponse memberDeleteResponse = memberService.deleteMember(
-            loginUser.getMemberId());
+                loginUser.getMemberId());
         return ResponseEntity.ok(memberDeleteResponse);
     }
 
     @Operation(summary = "사용자 상태 조회 API",
-        description = "사용자의 상태를 조회<br>"
-            + "memberStatus : [PROFILE, APPROVAL, STYLE, BOOK, COMPLETED, DELETED, BLOCKED, MATCHING_DISABLED, REPORTED]")
+            description = "사용자의 상태를 조회<br>"
+                    + "memberStatus : [PROFILE, APPROVAL, STYLE, BOOK, COMPLETED, DELETED, BLOCKED, MATCHING_DISABLED, REPORTED]")
     @GetMapping("/statuses")
     public ResponseEntity<MemberStatusResponse> readMemberStatus(
-        @Parameter(hidden = true) @User LoginUser loginUser) {
+            @Parameter(hidden = true) @User LoginUser loginUser) {
         MemberStatusResponse memberStatusResponse = memberService.readMemberStatus(
-            loginUser.getMemberId());
+                loginUser.getMemberId());
         return ResponseEntity.ok(memberStatusResponse);
     }
 
     @Operation(summary = "사용자 Bookmark 개수 조회", description = "책갈피 개수 조회 (기존:'/members/postcards')")
     @GetMapping("/bookmarks")
     public ResponseEntity<Integer> memberBookmarkCount(
-        @Parameter(hidden = true) @User LoginUser loginUser) {
+            @Parameter(hidden = true) @User LoginUser loginUser) {
         return ResponseEntity.ok(
                 memberBookmarkService.getMemberBookmarkCount(loginUser.getMemberId()));
     }
 
+    @Operation(summary = "애드몹 시청", description = "애드몹 시청 종료 후 해당 API 호출")
+    @PostMapping("/me/admob")
+    public void watchAdmob(
+            @Parameter(hidden = true) @User LoginUser loginUser
+    ) {
+        memberBookmarkService.updateBookmarkByAdmob(loginUser.getMemberId());
+    }
+
     @GetMapping("/same-book-members")
     public ResponseEntity<Page<MemberBookProfileResponse>> sameBookMembersPage(
-        @Parameter(hidden = true) @User LoginUser loginUser,
-        @ModelAttribute MemberBookProfileRequestDto memberBookProfileRequestDto,
-        Pageable pageable) {
+            @Parameter(hidden = true) @User LoginUser loginUser,
+            @ModelAttribute MemberBookProfileRequestDto memberBookProfileRequestDto,
+            Pageable pageable) {
         List<MemberBookProfileResponse> memberBookProfileResponseList = memberProfileService.findSameBookMembers(
-            loginUser.getMemberId(), memberBookProfileRequestDto);
+                loginUser.getMemberId(), memberBookProfileRequestDto);
         if (pageable == null) {
             pageable = PageRequest.of(0, 0);
         }
@@ -106,19 +115,19 @@ public class MemberController {
             memberBookProfileResponsePage = Page.empty();
         } else {
             memberBookProfileResponsePage = new PageImpl<>(
-                memberBookProfileResponseList.subList(start, end), pageable,
-                memberBookProfileResponseList.size());
+                    memberBookProfileResponseList.subList(start, end), pageable,
+                    memberBookProfileResponseList.size());
         }
         return ResponseEntity.ok(memberBookProfileResponsePage);
     }
 
     @GetMapping("/all-other-members")
     public ResponseEntity<Page<MemberBookProfileResponse>> getAllMembersProfile(
-        @Parameter(hidden = true) @User LoginUser loginUser,
-        @ModelAttribute MemberBookProfileRequestDto memberBookProfileRequestDto,
-        Pageable pageable) {
+            @Parameter(hidden = true) @User LoginUser loginUser,
+            @ModelAttribute MemberBookProfileRequestDto memberBookProfileRequestDto,
+            Pageable pageable) {
         List<MemberBookProfileResponse> result = memberProfileService.getAllMembers(
-            loginUser.getMemberId(), memberBookProfileRequestDto);
+                loginUser.getMemberId(), memberBookProfileRequestDto);
         if (pageable == null) {
             pageable = PageRequest.of(0, 0);
         }
@@ -130,8 +139,8 @@ public class MemberController {
             memberBookProfileResponsePage = Page.empty();
         } else {
             memberBookProfileResponsePage = new PageImpl<>(
-                result.subList(start, end), pageable,
-                result.size());
+                    result.subList(start, end), pageable,
+                    result.size());
         }
         return ResponseEntity.ok(memberBookProfileResponsePage);
     }
