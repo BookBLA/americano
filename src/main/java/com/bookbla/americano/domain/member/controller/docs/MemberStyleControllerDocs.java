56469fb0be1d56699bb13797b83113b6584a4c20
iamjooon2 <iamjooon2@gmail.com>
2024-08-25 19:50:29 +0900

    feat: 기본 프로필 이미지를 업데이트 하는 기능

--- src/main/java/com/bookbla/americano/domain/member/controller/docs/MemberStyleControllerDocs.java
+++ src/main/java/com/bookbla/americano/domain/member/controller/docs/MemberStyleControllerDocs.java
@@ -2,16 +2,19 @@ package com.bookbla.americano.domain.member.controller.docs;
 
 import com.bookbla.americano.base.resolver.LoginUser;
 import com.bookbla.americano.base.resolver.User;
+import com.bookbla.americano.domain.member.controller.dto.request.MemberProfileImageTypeUpdateRequest;
 import com.bookbla.americano.domain.member.controller.dto.request.MemberStyleCreateRequest;
 import com.bookbla.americano.domain.member.controller.dto.request.MemberStyleUpdateRequest;
 import com.bookbla.americano.domain.member.controller.dto.response.MemberStyleResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -41,4 +44,14 @@ public interface MemberStyleControllerDocs {
             @RequestBody @Valid MemberStyleUpdateRequest memberStyleUpdateRequest
     );
 
+    @Operation(summary = "회원 스타일 수정")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "수정 성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 프로필 이미지 id")
+    })
+    @PatchMapping
+    ResponseEntity<Void> updateMemberStyleProfileImageType(
+            @Parameter(hidden = true) @User LoginUser loginUser,
+            @RequestBody @Valid MemberProfileImageTypeUpdateRequest request
+    );
 }
