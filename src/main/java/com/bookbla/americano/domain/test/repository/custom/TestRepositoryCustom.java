830bac0406e1a6059c23b7601dc651c991793c46
iamjooon2 <iamjooon2@gmail.com>
2024-09-03 03:32:42 +0900

    fix: 예시 코드 제거

--- src/main/java/com/bookbla/americano/domain/test/repository/custom/TestRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.bookbla.americano.domain.test.repository.custom;
-
-import com.bookbla.americano.domain.test.repository.entity.TestEntity;
-import java.util.List;
-
-public interface TestRepositoryCustom {
-
-    List<TestEntity> findByContents(String contents);
-}
