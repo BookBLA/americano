663ca7001079802121efd9b994b819f7dd3c552b
khyu2 <66515313+Krockseed@users.noreply.github.com>
2024-09-02 17:43:23 +0900

    test: 엽서 보내는 테스트 코드 작성

--- src/test/java/com/bookbla/americano/domain/postcard/service/PostcardServiceTest.java
+++ src/test/java/com/bookbla/americano/domain/postcard/service/PostcardServiceTest.java
@@ -2,14 +2,22 @@ package com.bookbla.americano.domain.postcard.service;
 
 import com.bookbla.americano.base.exception.BaseException;
 import com.bookbla.americano.domain.member.repository.MemberBlockRepository;
+import com.bookbla.americano.domain.member.repository.MemberBookmarkRepository;
 import com.bookbla.americano.domain.member.repository.MemberRepository;
 import com.bookbla.americano.domain.member.repository.entity.Member;
 import com.bookbla.americano.domain.member.repository.entity.MemberBlock;
+import com.bookbla.americano.domain.member.repository.entity.MemberBookmark;
 import com.bookbla.americano.domain.postcard.controller.dto.response.PostcardSendValidateResponse;
 import com.bookbla.americano.domain.postcard.enums.PostcardStatus;
 import com.bookbla.americano.domain.postcard.exception.PostcardExceptionType;
 import com.bookbla.americano.domain.postcard.repository.PostcardRepository;
+import com.bookbla.americano.domain.postcard.repository.PostcardTypeRepository;
 import com.bookbla.americano.domain.postcard.repository.entity.Postcard;
+import com.bookbla.americano.domain.postcard.repository.entity.PostcardType;
+import com.bookbla.americano.domain.postcard.service.dto.request.SendPostcardRequest;
+import com.bookbla.americano.domain.postcard.service.dto.response.SendPostcardResponse;
+import com.bookbla.americano.domain.quiz.repository.QuizQuestionRepository;
+import com.bookbla.americano.domain.quiz.repository.entity.QuizQuestion;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -40,6 +48,59 @@ class PostcardServiceTest {
 
     @Autowired
     private MemberBlockRepository memberBlockRepository;
+
+    @Autowired
+    private MemberBookmarkRepository bookmarkRepository;
+
+    @Autowired
+    private QuizQuestionRepository quizQuestionRepository;
+
+    @Autowired
+    private PostcardTypeRepository postcardTypeRepository;
+
+    @Test
+    void 문제를_맞추면_엽서를_보낼_수_있다() throws Exception {
+        //given
+        Member sendMember = memberRepository.save(Member.builder().build());
+        Member reciveMember = memberRepository.save(Member.builder().build());
+        MemberBookmark memberBookmark = MemberBookmark.builder()
+                .member(sendMember)
+                .bookmarkCount(100).build();
+        bookmarkRepository.save(memberBookmark);
+        quizQuestionRepository.save(QuizQuestion.builder().firstChoice("answer").build());
+        postcardTypeRepository.save(PostcardType.builder().build());
+
+        SendPostcardRequest.QuizAnswer quizAnswer = new SendPostcardRequest.QuizAnswer(1L, "answer");
+        SendPostcardRequest request = new SendPostcardRequest(quizAnswer, 1L, reciveMember.getId(), "memberReply");
+
+        //when
+        SendPostcardResponse response = postcardService.send(sendMember.getId(), request);
+
+        //then
+        assertThat(response.getIsSendSuccess()).isTrue();
+    }
+
+    @Test
+    void 문제를_틀리면_엽서를_보낼_수_없다() throws Exception {
+        //given
+        Member sendMember = memberRepository.save(Member.builder().build());
+        Member reciveMember = memberRepository.save(Member.builder().build());
+        MemberBookmark memberBookmark = MemberBookmark.builder()
+                .member(sendMember)
+                .bookmarkCount(100).build();
+        bookmarkRepository.save(memberBookmark);
+        quizQuestionRepository.save(QuizQuestion.builder().firstChoice("answer").build());
+        postcardTypeRepository.save(PostcardType.builder().build());
+
+        SendPostcardRequest.QuizAnswer quizAnswer = new SendPostcardRequest.QuizAnswer(1L, "Wrong answer");
+        SendPostcardRequest request = new SendPostcardRequest(quizAnswer, 1L, reciveMember.getId(), "memberReply");
+
+        //when
+        SendPostcardResponse response = postcardService.send(sendMember.getId(), request);
+
+        //then
+        assertThat(response.getIsSendSuccess()).isFalse();
+    }
 
     @Test
     void 받는_사람이_보내는_사람을_차단했다면_엽서를_보낼_수_없다() {
