94d2452db572f5e46f8c8be94467d634761eff05
iamjooon2 <iamjooon2@gmail.com>
2024-09-09 00:22:22 +0900

    test: Fixture 사용 제거

--- src/test/java/com/bookbla/americano/domain/member/repository/MemberBookmarkRepositoryTest.java
+++ src/test/java/com/bookbla/americano/domain/member/repository/MemberBookmarkRepositoryTest.java
@@ -2,6 +2,7 @@ package com.bookbla.americano.domain.member.repository;
 
 import com.bookbla.americano.base.config.JpaConfig;
 import com.bookbla.americano.base.config.QuerydslConfig;
+import com.bookbla.americano.domain.member.repository.entity.Member;
 import com.bookbla.americano.domain.member.repository.entity.MemberBookmark;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -11,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
-import static com.bookbla.americano.fixture.Fixture.스타일_등록_완료_남성_고도리;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -45,7 +45,7 @@ class MemberBookmarkRepositoryTest {
         for (int i = 0; i < 100; i++) {
             sut.save(MemberBookmark.builder()
                     .admobCount(i)
-                    .member(memberRepository.save(스타일_등록_완료_남성_고도리))
+                    .member(memberRepository.save(Member.builder().build()))
                     .build());
         }
     }
