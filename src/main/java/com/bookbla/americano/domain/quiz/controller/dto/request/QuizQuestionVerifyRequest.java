6748b4b2c2bf24a968ef1bd04e5c79caff3ce04a
khyu2 <66515313+Krockseed@users.noreply.github.com>
2024-09-08 16:20:39 +0900

    feat: QuizQuestionVerifyRequest Validation 추가, 엽서 테스트 코드 수정

--- src/main/java/com/bookbla/americano/domain/quiz/controller/dto/request/QuizQuestionVerifyRequest.java
+++ src/main/java/com/bookbla/americano/domain/quiz/controller/dto/request/QuizQuestionVerifyRequest.java
@@ -5,11 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class QuizQuestionVerifyRequest {
 
+    @NotNull(message = "퀴즈 아이디가 입력되지 않았습니다.")
     private Long quizId;
+
+    @NotNull(message = "퀴즈 정답이 입력되지 않았습니다.")
     private String quizAnswer;
 }
