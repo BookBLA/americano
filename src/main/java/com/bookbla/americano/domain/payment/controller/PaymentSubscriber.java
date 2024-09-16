00c029f83942c4786b26442aeb08caa6e68a7469
iamjooon2 <iamjooon2@gmail.com>
2024-09-16 18:44:58 +0900

    feat: 애플 응답값 분리

--- src/main/java/com/bookbla/americano/domain/payment/controller/PaymentSubscriber.java
+++ src/main/java/com/bookbla/americano/domain/payment/controller/PaymentSubscriber.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RequestMapping("/payments")
 @RestController
 public class PaymentSubscriber {
 
@@ -18,7 +20,12 @@ public class PaymentSubscriber {
     public ResponseEntity<Void> receiveAppleNotification(
             @RequestBody AppleNotificationRequest request
     ) {
-        paymentService.receiveAppleNotification(request.getSignedPayload());
-        return ResponseEntity.ok().build();
+        // 애플로부터 들어온 환불 정보는 모두 저장해두고 싶었으나
+        // 예외가 터지면 트랜잭션 커밋이 안 돼, 일단 이렇게 구현해놨습니다
+        boolean isSuccess = paymentService.receiveAppleNotification(request.getSignedPayload());
+        if (isSuccess) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
