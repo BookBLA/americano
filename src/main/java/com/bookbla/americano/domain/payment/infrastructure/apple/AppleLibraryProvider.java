ef2f3171d6c03ab4cc7981b40879f26fcfffd749
iamjooon2 <iamjooon2@gmail.com>
2024-09-16 16:34:47 +0900

    fix: 애플 라이브러리 접근 제어자 변경

--- src/main/java/com/bookbla/americano/domain/payment/infrastructure/apple/AppleLibraryProvider.java
+++ src/main/java/com/bookbla/americano/domain/payment/infrastructure/apple/AppleLibraryProvider.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Component
 @Slf4j
-public class AppleLibraryProvider {
+class AppleLibraryProvider {
 
     private final ApplePaymentsConfig applePaymentsConfig;
 
