46872c3742c32418b20e41504211b7315c54ef81
iamjooon2 <iamjooon2@gmail.com>
2024-09-16 15:57:54 +0900

    feat: 로깅 추가

--- src/main/java/com/bookbla/americano/domain/payment/infrastructure/apple/AppleTokenProvider.java
+++ src/main/java/com/bookbla/americano/domain/payment/infrastructure/apple/AppleTokenProvider.java
@@ -5,12 +5,14 @@ import java.util.Base64;
 import com.bookbla.americano.domain.payment.infrastructure.apple.tokens.DecodedTokenPayload;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 class AppleTokenProvider {
 
     private static final String JWT_ELEMENT_DELIMITER = "\\.";
@@ -25,6 +27,7 @@ class AppleTokenProvider {
         try {
             return objectMapper.readValue(payload, DecodedTokenPayload.class);
         } catch (Exception e) {
+            log.error(e.getMessage());
             throw new RuntimeException("토큰 디코딩 중 예외가 발생했습니다.", e);
         }
     }
