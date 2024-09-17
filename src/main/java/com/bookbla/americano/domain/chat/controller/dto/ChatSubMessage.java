4b1633a04e6125d5fce19b915e54642fa9d7fdd6
Ryugi62 <xorjf1027@naer.com>
2024-09-17 22:50:16 +0900

    fix: chat handler

--- src/main/java/com/bookbla/americano/domain/chat/controller/dto/ChatSubMessage.java
+++ src/main/java/com/bookbla/americano/domain/chat/controller/dto/ChatSubMessage.java
@@ -1,5 +1,6 @@
 package com.bookbla.americano.domain.chat.controller.dto;
 
+import com.bookbla.americano.domain.chat.enums.SendChatStatus;
 import com.bookbla.americano.domain.chat.repository.entity.Chat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +19,15 @@ public class ChatSubMessage {
     private Long chatRoomId;
     private LocalDateTime sendTime;
     private Boolean isRead;
+    private SendChatStatus status;
+
+    public void setStatus(SendChatStatus status) {
+        this.status = status;
+    }
+
+    public void setRead(Boolean read) {
+        isRead = read;
+    }
 
     public static ChatSubMessage from(Chat chat) {
         return ChatSubMessage.builder()
@@ -26,6 +36,14 @@ public class ChatSubMessage {
                 .senderId(chat.getSender().getId())
                 .sendTime(chat.getSendTime())
                 .isRead(chat.getIsRead())
+                .build();
+    }
+    public static ChatSubMessage from(ChatPubMessage pub) {
+        return ChatSubMessage.builder()
+                .chatRoomId(pub.getChatRoomId())
+                .content(pub.getContent())
+                .senderId(pub.getSenderId())
+                .sendTime(pub.getSendTime())
                 .build();
     }
 }
