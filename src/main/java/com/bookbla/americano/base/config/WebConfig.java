b928c12392f56a77fe64307b3efde3585ae7a2cc
iamjooon2 <iamjooon2@gmail.com>
2024-09-20 07:25:34 +0900

    feat: 로깅 인터셉터 작용

--- src/main/java/com/bookbla/americano/base/config/WebConfig.java
+++ src/main/java/com/bookbla/americano/base/config/WebConfig.java
@@ -1,15 +1,28 @@
 package com.bookbla.americano.base.config;
 
 import com.bookbla.americano.base.log.RequestStorage;
+import com.bookbla.americano.base.log.query.LoggingInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
+@Component
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
