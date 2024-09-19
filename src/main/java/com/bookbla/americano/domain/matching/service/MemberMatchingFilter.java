bc27d841030b8796c1a8fb612da87912bda3aaa4
leevigong <dddddaeun88.88@gmail.com>
2024-09-19 16:07:48 +0900

    feat: 최초가입 + 학생증 상태 조건 필터링

--- src/main/java/com/bookbla/americano/domain/matching/service/MemberMatchingFilter.java
+++ src/main/java/com/bookbla/americano/domain/matching/service/MemberMatchingFilter.java
@@ -1,11 +1,47 @@
 package com.bookbla.americano.domain.matching.service;
 
+import com.bookbla.americano.domain.member.repository.MemberVerifyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class MemberMatchingFilter {
 
+    private final MemberVerifyRepository memberVerifyRepository;
 
+    public List<Map<Long, Long>> filter(List<Map<Long, Long>> matchingMembers) {
+
+        /** 학생증 상태 필터링 **/
+
+        Set<Long> filteringMemberIds = new HashSet<>();
+
+        // 중복 제외한 matchingMemberId (한명의 회원당 여러 책을 동록했을 수 있기때문에 중복 member id 값이 있을 수 있음)
+        for (Map<Long, Long> matchingMemberId : matchingMembers) {
+            filteringMemberIds.addAll(matchingMemberId.keySet());
+        }
+        // filteringMemberIds 필터링 -> filteredMatchMemberIds
+        List<Long> filteredMatchMemberIds = memberVerifyRepository.findStudentIdByVerify(filteringMemberIds);
+
+/*        List<Map<Long, Long>> filteredMatchMembers = matchingMembers.stream()
+                .filter(map -> map.keySet().stream().anyMatch(filteredMatchMemberIds::contains))
+                .collect(Collectors.toList());
+*/
+
+        List<Map<Long, Long>> filteredMatchMembers = new ArrayList<>();
+        for (Map<Long, Long> match : matchingMembers) {
+
+            // filteredMatchMemberIds에 포함된 키를 가진 match만 추가
+            for (Long key : match.keySet()) {
+                if (filteredMatchMemberIds.contains(key)) {
+                    filteredMatchMembers.add(match);
+                    break;  // 하나라도 조건을 만족하면 해당 Map을 추가하고 반복 종료
+                }
+            }
+        }
+
+        return filteredMatchMembers;
+    }
 }
---
+++ src/main/java/com/bookbla/americano/domain/member/repository/custom/impl/MemberVerifyRepositoryCustomImpl.java
@@ -0,0 +1,47 @@
+package com.bookbla.americano.domain.member.repository.custom.impl;
+
+import com.bookbla.americano.domain.member.enums.MemberVerifyStatus;
+import com.bookbla.americano.domain.member.enums.MemberVerifyType;
+import com.bookbla.americano.domain.member.repository.custom.MemberVerifyRepositoryCustom;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+import static com.bookbla.americano.domain.member.repository.entity.QMember.member;
+import static com.bookbla.americano.domain.member.repository.entity.QMemberVerify.memberVerify;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberVerifyRepositoryCustomImpl implements MemberVerifyRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<Long> findStudentIdByVerify(Set<Long> filteringMemberId) {
+
+        LocalDateTime twoDaysAgo = LocalDateTime.now().minusDays(2);
+
+        return queryFactory
+                .select(memberVerify.memberId)
+                .from(memberVerify)
+                .join(member).on(memberVerify.memberId.eq(member.id))
+                .where(
+                        (memberVerify.memberId.in(filteringMemberId)),
+                        (member.createdAt.before(twoDaysAgo)).or(member.createdAt.eq(twoDaysAgo))
+                                .and(memberVerify.verifyType.eq(MemberVerifyType.STUDENT_ID))
+                                .and(memberVerify.verifyStatus.eq(MemberVerifyStatus.SUCCESS)),
+
+                        (member.createdAt.after(twoDaysAgo)).or(member.createdAt.eq(twoDaysAgo))
+                                .and(memberVerify.verifyType.eq(MemberVerifyType.STUDENT_ID))
+                                .and(memberVerify.verifyStatus.eq(MemberVerifyStatus.SUCCESS))
+                                .or(memberVerify.verifyStatus.eq(MemberVerifyStatus.PENDING))
+                                .or(memberVerify.verifyStatus.eq(MemberVerifyStatus.FAIL))
+                )
+                .fetch();
+    }
+
+}
