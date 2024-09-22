d17e96fea9241a3b55a3a992a937dcac6a90da0e
khyu2 <66515313+Krockseed@users.noreply.github.com>
2024-09-23 03:10:01 +0900

    fix: MemberMatchingFilter 중복되는 메소드 추출

--- src/main/java/com/bookbla/americano/domain/matching/service/MemberMatchingFilter.java
+++ src/main/java/com/bookbla/americano/domain/matching/service/MemberMatchingFilter.java
@@ -5,6 +5,7 @@ import com.bookbla.americano.domain.member.repository.MemberBlockRepository;
 import com.bookbla.americano.domain.member.repository.MemberVerifyRepository;
 import com.bookbla.americano.domain.postcard.repository.PostcardRepository;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -24,11 +25,7 @@ public class MemberMatchingFilter {
      * 학생증 상태 확인 필터링
      */
     public List<MatchedInfo> memberVerifyFiltering(List<MatchedInfo> matchingMembers) {
-        Set<Long> filteringMemberIds = new HashSet<>();
-
-        matchingMembers.stream()
-                .map(MatchedInfo::getMatchedMemberId)
-                .forEach(filteringMemberIds::add);
+        Set<Long> filteringMemberIds = extractFilteringMemberIds(matchingMembers);
 
         // filteringMemberIds 필터링 -> filteredMatchMemberIds
         List<Long> filteredMatchMemberIds = memberVerifyRepository.getMemberIdsByStudentIdVerify(filteringMemberIds);
@@ -42,12 +39,7 @@ public class MemberMatchingFilter {
      * 엽서 거절 조건 필터링
      */
     public List<MatchedInfo> memberRefusedAtFiltering(Long sendMemberId, List<MatchedInfo> matchingMembers) {
-        Set<Long> filteringMemberIds = new HashSet<>();
-
-        // 중복 제외한 matchingMemberId (한명의 회원당 여러 책을 동록했을 수 있기때문에 중복 member id 값이 있을 수 있음)
-        matchingMembers.stream()
-                .map(MatchedInfo::getMatchedMemberId)
-                .forEach(filteringMemberIds::add);
+        Set<Long> filteringMemberIds = extractFilteringMemberIds(matchingMembers);
 
         // 앱 사용자로 부터 받은 엽서를 거절한지 14일이 안된 회원 = 추천되면 안되는 회원
         List<Long> filteredMatchMemberIds = postcardRepository.getReceiveIdsRefusedAt(sendMemberId, filteringMemberIds);
@@ -62,11 +54,7 @@ public class MemberMatchingFilter {
      * 차단된 회원 필터링
      */
     public List<MatchedInfo> memberBlockedFiltering(Long appMemberId, List<MatchedInfo> matchingMembers) {
-        Set<Long> filteringMemberIds = new HashSet<>();
-
-        matchingMembers.stream()
-                .map(MatchedInfo::getMatchedMemberId)
-                .forEach(filteringMemberIds::add);
+        Set<Long> filteringMemberIds = extractFilteringMemberIds(matchingMembers);
 
         // 앱 사용자로 부터 차단된 회원 = 추천되면 안되는 회원
         List<Long> filteredMatchMemberIds = memberBlockRepository.getBlockedMemberIdsByBlockerMemberId(appMemberId, filteringMemberIds);
@@ -77,4 +65,12 @@ public class MemberMatchingFilter {
                 .collect(Collectors.toList());
     }
 
+    private static @NotNull Set<Long> extractFilteringMemberIds(List<MatchedInfo> matchingMembers) {
+        Set<Long> filteringMemberIds = new HashSet<>();
+
+        matchingMembers.stream()
+                .map(MatchedInfo::getMatchedMemberId)
+                .forEach(filteringMemberIds::add);
+        return filteringMemberIds;
+    }
 }
