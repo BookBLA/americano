368b88d28dd4b4e19772bcbb61b72142ad71550e
khyu2 <66515313+Krockseed@users.noreply.github.com>
2024-09-24 00:11:24 +0900

    fix: request 기본 생성자 추가, 매칭 필터 stream API로 변경

--- src/main/java/com/bookbla/americano/domain/matching/controller/dto/request/RejectMemberRequest.java
+++ src/main/java/com/bookbla/americano/domain/matching/controller/dto/request/RejectMemberRequest.java
@@ -2,10 +2,12 @@ package com.bookbla.americano.domain.matching.controller.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class RejectMemberRequest {
 
