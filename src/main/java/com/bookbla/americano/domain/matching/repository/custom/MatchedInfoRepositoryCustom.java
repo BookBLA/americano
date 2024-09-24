fda0abaa28fa2679fbd23bbad95ba6a002db38a0
khyu2 <66515313+Krockseed@users.noreply.github.com>
2024-09-25 03:56:10 +0900

    feat: 엔티티 분리에 따라 MatchedInfoRepository 변경사항 적용

---
+++ src/main/java/com/bookbla/americano/domain/matching/repository/custom/MatchedInfoRepositoryCustom.java
@@ -0,0 +1,12 @@
+package com.bookbla.americano.domain.matching.repository.custom;
+
+import com.bookbla.americano.domain.matching.repository.entity.MatchedInfo;
+
+import java.util.List;
+
+public interface MatchedInfoRepositoryCustom {
+
+    List<MatchedInfo> findAllByMemberMatchingId(Long memberMatchingId);
+
+    List<MatchedInfo> getAllByDesc(Long memberMatchingId);
+}
