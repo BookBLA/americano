fe17d4bfe25da51ff386001bb0d2cf0657911ec0
khyu2 <66515313+Krockseed@users.noreply.github.com>
2024-09-25 17:50:25 +0900

    fix: refresh 시 다른 사용자를 반환하는 문제 해결

--- src/main/java/com/bookbla/americano/domain/matching/repository/MatchedInfoRepository.java
+++ src/main/java/com/bookbla/americano/domain/matching/repository/MatchedInfoRepository.java
@@ -5,4 +5,6 @@ import com.bookbla.americano.domain.matching.repository.entity.MatchedInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MatchedInfoRepository extends JpaRepository<MatchedInfo, Long>, MatchedInfoRepositoryCustom {
+
+    void deleteByMemberIdAndMatchedMemberIdAndMatchedMemberBookId(Long memberId, Long matchedMemberId, Long matchedMemberBookId);
 }
