9988862573656c3d2fac634b3eece3e5646c1a8d
khyu2 <66515313+Krockseed@users.noreply.github.com>
2024-09-26 13:54:59 +0900

    fix: 잘못된 delete query 수정

--- src/main/java/com/bookbla/americano/domain/matching/repository/custom/impl/MatchedInfoRepositoryImpl.java
+++ src/main/java/com/bookbla/americano/domain/matching/repository/custom/impl/MatchedInfoRepositoryImpl.java
@@ -59,12 +59,10 @@ public class MatchedInfoRepositoryImpl implements MatchedInfoRepositoryCustom {
                         matchedInfo.memberId.in(
                                 JPAExpressions.select(member.id)
                                         .from(member)
-                                        .where(
-                                                member.memberStatus.ne(MemberStatus.DELETED),
-                                                member.memberStatus.ne(MemberStatus.BLOCKED),
-                                                member.memberStatus.ne(MemberStatus.MATCHING_DISABLED),
-                                                member.memberStatus.ne(MemberStatus.REPORTED)
-                                        )
+                                        .where(member.memberStatus.eq(MemberStatus.DELETED)
+                                                .or(member.memberStatus.eq(MemberStatus.BLOCKED))
+                                                .or(member.memberStatus.eq(MemberStatus.MATCHING_DISABLED))
+                                                .or(member.memberStatus.eq(MemberStatus.REPORTED)))
                         )
                 )
                 .execute();
