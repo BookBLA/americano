ba4b2e4c71af4ae3865119194b887e28003b4a78
GinToFim <shlf6673@gmail.com>
2024-10-12 21:49:44 +0900

    fix: 가입승인 알림 문구 수정

--- src/main/java/com/bookbla/americano/domain/notification/enums/PushAlarmForm.java
+++ src/main/java/com/bookbla/americano/domain/notification/enums/PushAlarmForm.java
@@ -10,7 +10,7 @@ public enum PushAlarmForm {
     POSTCARD_SEND("띵동~\uD83D\uDC8C 엽서가 도착했어요!", "%s님이 엽서를 보냈어요! 지금 확인해 보세요~\uD83E\uDD70"),
     POSTCARD_ACCEPT("축하합니다\uD83E\uDD73\uD83E\uDD73 매칭에 성공하셨습니다~!!\uD83D\uDC95", "%s님이 엽서를 수락했어요! 지금 바로 채팅해보세요~\uD83E\uDD70"),
 
-    ADMIN_VERIFICATION_ACCEPT("💌가입 승인이 완료되었습니다!", "스타일을 입력하고 매칭을 시작해보세요"),
+    ADMIN_VERIFICATION_ACCEPT("💌가입 승인이 완료되었습니다!", "매칭을 시작해보세요!"),
     ADMIN_PROFILE_IMAGE_REJECT("\uD83D\uDE22프로필을 다시 등록해주세요!", "프로필 사진을 다시 등록 후 매칭을 시작해보세요!"),
     ADMIN_STUDENT_ID_IMAGE_ACCEPT("학생증  인증에 성공했어요!", "무료 책갈피 35개 받고 매칭을 시작해 보세요!"),
     ADMIN_STUDENT_ID_IMAGE_REJECT("\uD83D\uDE22학생증을 다시 등록해주세요!", "학생증의 모든 정보(이름,학번,학과,얼굴)가 선명하게 보이도록 해주세요! "),
