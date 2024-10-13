299c63cb5bf90c105423793ee9c488a7c850d1e2
leevigong <dddddaeun88.88@gmail.com>
2024-10-14 01:26:24 +0900

    style: 예외 메세지 변경

--- src/main/java/com/bookbla/americano/domain/matching/exception/MemberMatchingExceptionType.java
+++ src/main/java/com/bookbla/americano/domain/matching/exception/MemberMatchingExceptionType.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum MemberMatchingExceptionType implements ExceptionType {
 
-    MATCHING_INFO_DOESNT_EXIST(HttpStatus.NOT_FOUND, "member_matching_001", "매칭될 정보가 존재하지 않습니다."),
+    MATCHING_INFO_DOESNT_EXIST(HttpStatus.NOT_FOUND, "member_matching_001", "해당 매칭 정보가 존재하지 않습니다."),
     NOT_FOUND_MATCHING(HttpStatus.NOT_FOUND, "member_matching_002", "매칭이 존재하지 않습니다."),
     EXCEED_MAX_RECOMMENDATION(HttpStatus.BAD_REQUEST, "member_matching_003", "하루 최대 추천 회원 수를 초과하였습니다."),
     ALREADY_MATCHED(HttpStatus.BAD_REQUEST, "member_matching_004", "이미 매칭된 회원입니다."),
