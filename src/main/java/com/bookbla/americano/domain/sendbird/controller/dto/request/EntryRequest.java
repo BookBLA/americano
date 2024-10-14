bcd49269334f85c92b475d606f5e81c8baed91c5
leevigong <dddddaeun88.88@gmail.com>
2024-10-15 01:14:58 +0900

    refactor: dto 구분

--- src/main/java/com/bookbla/americano/domain/sendbird/controller/dto/request/EntryRequest.java
+++ src/main/java/com/bookbla/americano/domain/sendbird/controller/dto/request/EntryRequest.java
@@ -1,4 +1,4 @@
-package com.bookbla.americano.domain.sendbird.controller.dto;
+package com.bookbla.americano.domain.sendbird.controller.dto.request;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
