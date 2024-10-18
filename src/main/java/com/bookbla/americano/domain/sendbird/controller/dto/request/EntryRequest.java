33a00f471812139c4934f81e797e7c7b6c7e880d
leevigong <dddddaeun88.88@gmail.com>
2024-10-18 17:20:43 +0900

    refactor: 사용하지 않는 필드 삭제

--- src/main/java/com/bookbla/americano/domain/sendbird/controller/dto/request/EntryRequest.java
+++ src/main/java/com/bookbla/americano/domain/sendbird/controller/dto/request/EntryRequest.java
@@ -15,8 +15,5 @@ public class EntryRequest {
     @NotNull(message = "채팅 수락/거절시 책갈피 처리할 회원이 입력 되지 않았습니다")
     private Long targetMemberId;
 
-    @NotNull(message = "postcardId를 입력하지 않았습니다.")
-    private Long postcardId;
-
     private String channelUrl;
 }
